80604b2054c1356da96b6528325a69e58d25de23
JWillumsen89 <100369030+JWillumsen89@users.noreply.github.com>
2022-03-03 10:05:16 +0100

    Added small text updates.

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,36 @@ import java.util.Scanner;
 
 public class Main {
 
+  String enterText() {
+    Scanner in = new Scanner(System.in);
+    System.out.print("Type text: ");
+    String text = in.nextLine();
+    text = text.toUpperCase();
+    System.out.print("You entered: " + text);
+    System.out.print("\nEnter shift value (0-29): ");
+    shiftValue();
+
+    return text;
+  }
+
+  String enterCodeText() {
+    Scanner in = new Scanner(System.in);
+    System.out.print("Type code text: ");
+    String decryptText = in.nextLine();
+    decryptText = decryptText.toUpperCase();
+    System.out.print("You entered: " + decryptText);
+    return decryptText;
+  }
+  //Shift value hardcoded! and modulus not made to circle alphabet, so last letters in alphabet cant be used.
+  int shiftValue() {
+    /*Scanner in = new Scanner(System.in);
+    System.out.print("\nEnter shift value (0-29): ");
+    int shift = in.nextInt();
+    System.out.println("You picked shift value: " + shift);*/
+    int shift = 3;
+    return shift;
+  }
+
   void userInterfaceStart() {
 
     System.out.println("1) \tCaesar");
@@ -40,36 +70,6 @@ public class Main {
     }
   }
 
-  String enterText() {
-    Scanner in = new Scanner(System.in);
-    System.out.print("Type text: ");
-    String text = in.nextLine();
-    text = text.toUpperCase();
-    System.out.print("You entered: " + text);
-    System.out.print("\nEnter shift value (0-29): ");
-    shiftValue();
-
-    return text;
-  }
-
-  String enterDecryptText() {
-    Scanner in = new Scanner(System.in);
-    System.out.print("Type code text: ");
-    String decryptText = in.nextLine();
-    decryptText = decryptText.toUpperCase();
-    System.out.print("You entered: " + decryptText);
-    return decryptText;
-  }
-//Shift value hardcoded! and modulus not made to circle alphabet, so last letters in alphabet cant be used.
-  int shiftValue() {
-    /*Scanner in = new Scanner(System.in);
-    System.out.print("\nEnter shift value (0-29): ");
-    int shift = in.nextInt();
-    System.out.println("You picked shift value: " + shift);*/
-    int shift = 3;
-    return shift;
-  }
-
   void caesarEncryptMenu() {
     System.out.println("Caesar Encrypting");
     caesarEncrypt();
@@ -82,6 +82,64 @@ public class Main {
     caesarDecrypt();
     System.out.println();
     whatNowMenu2();
+  }
+
+  void caesarEncrypt() {
+    String text = enterText();
+    int[] encryptedNumbers = encryptMethod(text);
+    String codeText = encryptedMethod(encryptedNumbers);
+    int shiftValueCaesar = shiftValue();
+    System.out.println("\nCode text: " + codeText);
+  }
+
+  void caesarDecrypt() {
+    String codeText = enterCodeText();
+    int[] encryptedNumbers = encryptMethod(codeText);
+    String decryptedText = decryptedMethod(encryptedNumbers);
+    int shiftValueCaesar = shiftValue();
+    System.out.println("\nDecrypted text: " + decryptedText);
+  }
+
+  public int[] encryptMethod(String text) {
+    int[] numbers = new int[text.length()];
+    for (int i = 0; i < text.length(); i++) {
+      char charAtIndex = text.charAt(i);
+      int indexOfChar = letterToNumber(charAtIndex);
+      numbers[i] = indexOfChar;
+    }
+    return numbers;
+  }
+
+  public String encryptedMethod(int[] numbers) {
+    String text = "";
+    char arrayNumbers;
+    for (int i = 0; i < numbers.length; i++) {
+      arrayNumbers = numberToLetter(numbers[i] + shiftValue());
+      text += arrayNumbers;
+    }
+    return text;
+  }
+
+  public String decryptedMethod(int[] numbers) {
+    String text = "";
+    char c;
+    for (int i = 0; i < numbers.length; i++) {
+      c = numberToLetter(numbers[i] - shiftValue());
+      text += c;
+    }
+    return text;
+  }
+
+  public char numberToLetter(int number) {
+    String alphabet = " ABCDEFGHIJKLMNOPQRSTVWXYZÆØÅ";
+    char letter = alphabet.charAt(number);
+    return letter;
+  }
+
+  public int letterToNumber(char letter) {
+    String alphabet = " ABCDEFGHIJKLMNOPQRSTVWXYZÆØÅ";
+    int number = alphabet.indexOf(letter);
+    return number;
   }
 
   void whatNowMenu1() {
@@ -118,70 +176,10 @@ public class Main {
     }
   }
 
-  void caesarEncrypt() {
-    String text = enterText();
-    int[] encryptedNumbers = encryptMethod(text);
-    String decryptedText = decryptedMethod(encryptedNumbers);
-    int shiftValueCaesar = shiftValue();
-    System.out.println("\nDecrypted text: " + decryptedText);
-  }
-
-  void caesarDecrypt() {
-    String decryptText = enterDecryptText();
-    int[] encryptedNumbers = encryptMethod(decryptText);
-    String decryptedText = encryptedMethod(encryptedNumbers);
-    int shiftValueCaesar = shiftValue();
-    System.out.println("\nEncrypted text: " + decryptedText);
-  }
-
-  public int[] encryptMethod(String text) {
-    int[] numbers = new int[text.length()];
-    for (int i = 0; i < text.length(); i++) {
-      char charAtIndex = text.charAt(i);
-      int indexOfChar = letterToNumber(charAtIndex);
-      numbers[i] = indexOfChar;
-    }
-    return numbers;
-  }
-
-  public String decryptedMethod(int[] numbers) {
-    String text = "";
-    char arrayNumbers;
-    for (int i = 0; i < numbers.length; i++) {
-      arrayNumbers = numberToLetter(numbers[i] + shiftValue());
-      text += arrayNumbers;
-    }
-    return text;
-  }
-
-  public String encryptedMethod(int[] numbers) {
-    String text = "";
-    char c;
-    for (int i = 0; i < numbers.length; i++) {
-      c = numberToLetter(numbers[i] - shiftValue());
-      text += c;
-    }
-    return text;
-  }
-
-  public char numberToLetter(int number) {
-    String alphabet = " ABCDEFGHIJKLMNOPQRSTVWXYZÆØÅ";
-    char letter = alphabet.charAt(number);
-    return letter;
-  }
-
-  public int letterToNumber(char letter) {
-    String alphabet = " ABCDEFGHIJKLMNOPQRSTVWXYZÆØÅ";
-    int number = alphabet.indexOf(letter);
-    return number;
-  }
-
-
   void go() {
     userInterfaceStart();
 
   }
-
 
   public static void main(String[] args) {
     new Main().go();
